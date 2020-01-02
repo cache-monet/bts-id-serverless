f68078a44db0536074055210ede53801e96eb4e7
cache-monet <remiaston@gmail.com>
2020-01-02 10:31:36 -0800

    cleanup

--- functions/identify/handler.py
+++ functions/identify/handler.py
@@ -2,8 +2,10 @@ import json
 import logging
 import os
 import sys
-# Set logging level
+# Set logging level and env vars
 logging.basicConfig(level=logging.INFO)
+SOURCE = os.environ['SOURCE']
+TARGET = os.environ['TARGET']
 # Unzip packaged dependencies
 try: 
   import unzip_requirements
@@ -20,17 +22,17 @@ def process(event, context):
   key = event['object']['key']
   [filename, extension] = key.split('.')
   processed_key = '{}_identified.{}'.format(filename, extension)
-  s3.download_file(Bucket='bts-identifier-source', Key=key, Filename=key)
+  s3.download_file(Bucket=SOURCE, Key=key, Filename=key)
   logging.info('{} downloaded'.format(key))
   identified = identify(key, processed_key, 'encodings.pickle')
-  s3.upload_file(Bucket='bts-identifier-target', Key=processed_key, Filename=processed_key)
+  s3.upload_file(Bucket=TARGET, Key=processed_key, Filename=processed_key)
   logging.info('{} uploaded'.format(processed_key))
   os.remove(key)
   os.remove(processed_key)
   return {
     'statusCode': 200,
-    'body': json.dumps('Hello from Lambda!')
+    'body': json.dumps('completed')
   }
 
 if __name__ == '__main__':
-  process({'object': {'key': 'test.png'}}, '')
+  process({'object': {'key': 'example.png'}}, '')
